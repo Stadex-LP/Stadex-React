4780df6912c7635c67ee7b1fbcbfa22c7f0e6e75
Dario-69 <57715903+Dario-69@users.noreply.github.com>
2023-03-28 01:51:23 +0200

    new design
    new design

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+import Dropdown from "components/dropdown";
+import { FiAlignJustify } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import navbarimage from "assets/img/layout/Navbar.png";
+import { BsArrowBarUp } from "react-icons/bs";
+import { FiSearch } from "react-icons/fi";
+import { RiMoonFill, RiSunFill } from "react-icons/ri";
+import {
+  IoMdNotificationsOutline,
+  IoMdInformationCircleOutline,
+} from "react-icons/io";
+import avatar from "assets/img/avatars/avatar4.png";
+
+const Navbar = (props) => {
+  const { onOpenSidenav, brandText } = props;
+  const [darkmode, setDarkmode] = React.useState(false);
+
+  return (
+    <nav className="sticky bg-white px-2 py-2 rounded-[20px] top-4 z-40 flex flex-row flex-wrap items-center justify-between">
+      
+      <div className="ml-[6px]">
+
+        <div className="h-6 w-[224px] pt-1">
+          <a
+            className="text-sm font-normal text-navy-700 hover:underline dark:text-white dark:hover:text-white"
+            href=" "
+          >
+            Pages
+            <span className="mx-1 text-sm text-navy-700 hover:text-navy-700 dark:text-white">
+              {" "}
+              /{" "}
+            </span>
+          </a>
+          <Link
+            className="text-sm font-normal capitalize text-navy-700 hover:underline dark:text-white dark:hover:text-white"
+            to="#"
+          >
+            {brandText}
+          </Link>
+        </div>
+
+        <p className="shrink text-[33px] capitalize text-navy-700 dark:text-white">
+          <Link
+            to="#"
+            className="font-bold capitalize hover:text-navy-700 dark:hover:text-white"
+          >
+            {brandText}
+          </Link>
+        </p>
+
+      </div>
+
+      <div className="relative mt-[3px] flex h-[61px] w-[355px] flex-grow justify-end gap-2 px-2 py-2 md:w-[365px] md:flex-grow-0 md:gap-1 xl:w-[365px] xl:gap-2">
+        {/* Profile & Dropdown */}
+        <Dropdown
+          button={
+            <img
+              className="h-10 w-10 rounded-full"
+              src={avatar}
+              alt="Elon Musk"
+            />
+          }
+          children={
+            <div className="flex h-48 w-56 flex-col justify-start rounded-[20px] bg-white bg-cover bg-no-repeat shadow-xl shadow-shadow-500 dark:!bg-navy-700 dark:text-white dark:shadow-none">
+              <div className="mt-3 ml-4">
+                <div className="flex items-center gap-2">
+                  <p className="text-sm font-bold text-navy-700 dark:text-white">
+                    👋 Hey, Adela
+                  </p>{" "}
+                </div>
+              </div>
+              <div className="mt-3 h-px w-full bg-gray-200 dark:bg-white/20 " />
+
+              <div className="mt-3 ml-4 flex flex-col">
+                <a
+                  href=" "
+                  className="text-sm text-gray-800 dark:text-white hover:dark:text-white"
+                >
+                  Profile Settings
+                </a>
+                <a
+                  href=" "
+                  className="mt-3 text-sm text-gray-800 dark:text-white hover:dark:text-white"
+                >
+                  Newsletter Settings
+                </a>
+                <a
+                  href=" "
+                  className="mt-3 text-sm font-medium text-red-500 hover:text-red-500"
+                >
+                  Log Out
+                </a>
+              </div>
+            </div>
+          }
+          classNames={"py-2 top-8 -left-[180px] w-max"}
+        />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
